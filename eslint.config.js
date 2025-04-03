c3e3e419ba0f8bc47ab6f9961ac497c417517ff8
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-04-03 22:30:58 +0700

    chore(linter): install and configure more ESLint and TypeScript
    Add more strict rules for TypeScript, set strict presets with type
    checking instead of normal recommended and install additional ESLint
    plugins for better code quality
    - eslint-plugin-jsx-a11y
    - eslint-plugin-react-x
    - eslint-plugin-react-dom
    
    Add configuration to let Prettier fix some rules by formatting instead
    of linting
    - eslint-config-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,48 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import configPrettier from 'eslint-config-prettier';
+import reactX from 'eslint-plugin-react-x';
+import reactDom from 'eslint-plugin-react-dom';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'vite.config.ts'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.stylisticTypeChecked,
+      ...tseslint.configs.strictTypeChecked,
+    ],
     files: ['**/*.{ts,tsx}'],
+    ...jsxA11y.flatConfigs.strict,
     languageOptions: {
+      ...jsxA11y.flatConfigs.strict.languageOptions,
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
+      'react-x': reactX,
+      'react-dom': reactDom,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      '@typescript-eslint/no-non-null-assertion': 'warn',
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      ...reactX.configs['recommended-type-checked'].rules,
+      ...reactDom.configs.recommended.rules,
+      ...configPrettier.rules,
     },
   },
-)
+);
