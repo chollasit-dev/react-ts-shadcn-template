9259c0230db65134671ecd27fc7f67a66cecb158
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-04-03 22:39:17 +0700

    chore(style): install Tailwind CSS for styling solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className='min-h-dvh content-center-safe bg-neutral-200'>
+      <section className='grid min-h-[90dvh] place-content-center-safe gap-4 bg-neutral-600 text-center text-white/85'>
+        <h1 className='text-5xl font-semibold'>Template</h1>
+        <div className='text-3xl'>
+          <span className='font-medium text-sky-400/80'>React </span>+
+          <span className='font-medium text-blue-400/90'> TypeScript SWC </span>
+          +<span className='font-medium text-sky-400/80'> Tailwind CSS </span>+
+          <span className='font-medium'> shadcn / ui </span>
+        </div>
+      </section>
+    </main>
+  );
 }
-
-export default App
